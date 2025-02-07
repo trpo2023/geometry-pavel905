4cd3a81c12e7d5ff6798f01a221e0e03fac006ad
Pavel <pav.mir8@gmail.com>
2025-02-08 01:42:08 +0700

    Удаление ненужных каталогов из ветки main

--- TRPO_Labs/Lab3/geometry-pavel905/geometry/include/triangle.h
+++
@@ -1,6 +0,0 @@
-#ifndef TRIANGLE_H
-#define TRIANGLE_H
-
-#include "geometry.h"
-
-#endif
--- TRPO_Labs/Lab3/geometry-pavel905/geometry/obj/src/app/main.d
+++
@@ -1,2 +0,0 @@
-obj/src/app/main.o: src/app/main.c include/wkt_parser.h \
- include/geometry.h include/geometry.h
